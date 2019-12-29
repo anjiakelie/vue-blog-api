65ed9e2f0697147f79425a514306d9ac7b04ed42
anjiakelie <1719635266@qq.com>
2019-12-29 22:49:21 +0800

    增加注册接口，id出问题了

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,5 +1,6 @@
 "use strict";
 const Controller = require("egg").Controller;
+const moment = require("moment");
 
 class HomeController extends Controller {
   async login() {
@@ -38,7 +39,58 @@ class HomeController extends Controller {
     }
 
     ctx.body = {
-      code: 12222
+      code: 1
+    };
+  }
+
+  async forgetpsw() {
+    const { ctx, app } = this;
+    let params = {};
+    const { account, pass, sms } = ctx.request.body;
+    const userPassWord = ctx.helper.md5(pass);
+    const userId = moment().format("YYYYMMDDHHmmss") + ctx.helper.rndNum(18);
+    console.log("userId-->", userId);
+    const conn = await app.mysql.beginTransaction(); // 初始化事务
+    const hasUser = await app.mysql.get("user", {
+      account
+    });
+    if (hasUser) {
+      ctx.body = {
+        code: 10,
+        msg: "该用户已存在!"
+      };
+      return;
+    }
+
+    try {
+      const result = await conn.insert("user", {
+        id: userId,
+        account,
+        password: userPassWord,
+        code: 1,
+        state: 10,
+        createTime: moment().format("YYYY-MM-DD HH:mm:ss")
+      });
+      await conn.commit(); // 提交事务
+      if (result.affectedRows === 1) {
+        ctx.body = {
+          code: 0,
+          msg: "提交成功"
+        };
+        return;
+      }
+    } catch (err) {
+      await conn.rollback(); // 一定记得捕获异常后回滚事务！！
+      ctx.body = {
+        code: -1,
+        msg: "提交失败"
+      };
+      ctx.logger.info("error--->", err);
+      throw err;
+    }
+    ctx.body = {
+      code: -1,
+      msg: "提交失败"
     };
   }
 }
