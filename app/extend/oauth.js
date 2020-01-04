8b67f0b1bd88a5bf2b09fc8c246aeb01c33476a6
kuangyj <kuangyj@hlpays.cn>
2020-01-04 21:42:59 +0800

    增加token的配置

---
+++ app/extend/oauth.js
@@ -0,0 +1,47 @@
+"use strict";
+const path = require("path");
+// const nconf = require('nconf');//用在mock的
+module.exports = app => {
+  // nconf.use('file',{
+  //   file:path.join(app.config.baseDir,'app/mock/db.json');
+  // })
+  class Model {
+    constructor(ctx) {
+      this.ctx = ctx;
+    }
+    async getClient(clientId, clientSecret) {
+      // const client = nconf.get('client');
+      if (
+        clientId !== clientId.clientId ||
+        clientSecret !== clientSecret.clientSecret
+      ) {
+        return;
+      }
+      return client;
+    }
+    async getUser(username, password) {
+      // const user = nconf.get("user");
+      if (username !== user.username || password !== user.password) {
+        return;
+      }
+      return { userId: user.id };
+    }
+    async getAccessToken(bearerToken) {
+      // const token = nconf.get("token");
+      token.accessTokenExpiresAt = new Date(token.accessTokenExpiresAt);
+      token.refreshTokenExpiresAt = new Date(token.refreshTokenExpiresAt);
+      // const user = nconf.get("user");
+      // const client = nconf.get("client");
+      // token.user = user;
+      // token.client = client;
+      return token;
+    }
+    async saveToken(token, client, user) {
+      const _token = Object.assign({}, token, { user }, { client });
+      // nconf.set("token", _token);
+      // nconf.save();
+      return _token;
+    }
+  }
+  return Model;
+};
